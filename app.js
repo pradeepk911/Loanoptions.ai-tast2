0b04db94e25f5d31df56b316cd607c5cc382a006
pradeepk911 <pkrishnamurt@student.unimelb.edu.au>
2022-11-13 22:53:56 +1100

    Fixed sort
    Updated the sort functionality to be case-insensitive.

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ function parseJSON(data){
     }
   });
 
-  APIs.sort();
-  APIs.reverse();
+  APIs.sort(function(a, b) {
+    if (a.toLowerCase() < b.toLowerCase()) return 1;
+    if (a.toLowerCase() > b.toLowerCase()) return -1;
+    return 0;
+  });
 
   APIs.forEach(element => {
     console.log(element);
